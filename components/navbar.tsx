1bd63126a5ebd6c022c114b493eab53cda1f8b6e
pradyumnsuper <pradyumnsuper@gmail.com>
2023-11-27 16:45:33 +0530

    as

---
+++ components/navbar.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import Link from "next/link";
+import { UserButton } from "@clerk/nextjs";
+import { Poppins } from "next/font/google";
+import { Sparkles } from "lucide-react";
+
+import { cn } from "@/lib/utils";
+import { MobileSidebar } from "@/components/mobile-sidebar";
+import { ModeToggle } from "@/components/mode-toggle";
+import { Button } from "@/components/ui/button";
+
+
+const font = Poppins({ weight: "600", subsets: ["latin"] });
+
+export const Navbar = () => {
+ 
+
+  
+
+  return ( 
+    <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 h-16 border-b border-primary/10 bg-secondary">
+      <div className="flex items-center">
+        <MobileSidebar  />
+        <Link href="/">
+          <h1 className={cn("hidden md:block text-xl md:text-3xl font-bold text-primary", font.className)}>
+            MyAIFriends.ai
+          </h1>
+        </Link>
+      </div>
+      <div className="flex items-center gap-x-3">
+      
+          <Button  size="sm" variant="premium">
+            Upgrade
+            <Sparkles className="h-4 w-4 fill-white text-white ml-2" />
+          </Button>
+        
+        <ModeToggle />
+        <UserButton afterSignOutUrl="/" />
+      </div>
+    </div>
+  );
+}
---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: [
+          "res.cloudinary.com",
+        ],
+      },
+
+
+
+}
+
+module.exports = nextConfig
+
+
